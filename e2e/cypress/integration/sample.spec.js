ee3678bd8b47e260e955cfbc9f8cfd74c6bdadfe
sj0x55 <383337+sj0x55@users.noreply.github.com>
2021-10-06 21:17:48 +0100

    Update cypress test description

--- e2e/cypress/integration/sample.spec.js
+++ e2e/cypress/integration/sample.spec.js
@@ -1,5 +1,5 @@
 describe('My First Test', () => {
-  it('Visits the Kitchen Sink', () => {
+  it('Visits each page', () => {
     cy.visit('/');
     cy.get('[data-testid=root-container]').should('be.visible');
     cy.get('nav').find('[data-testid=smartphones]').click();
